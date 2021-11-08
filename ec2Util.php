17c716f3bfbcc1125f35f6ef06630999f09c1f5a
Pranav S <psmallrounder@gmail.com>
2021-11-08 19:58:04 +0530

    Ec2 instance testing

--- ec2Util.php
+++ ec2Util.php
@@ -1,20 +1,8 @@
 <?php
-      // require 'vendor/autoload.php';
-      // // require 'dynamoDBUtil.php';
-      // use Aws\Ec2\Ec2Client;
-      // require 'cred.php';
-      //
-      // $credentials = new Aws\Credentials\Credentials($access_key, $secret_key);
-      //
-      // $ec2Client = new Aws\Ec2\Ec2Client([
-      //     'region' => 'us-west-2',
-      //     'version' => '2016-11-15',
-      //     'credentials' => $credentials
-      // ]);
-
-      echo "INSTANCE ID: " . @file_get_contents("http://instance-data/latest/meta-data/instance-id");
-      echo "<br>";
-      echo "AVAILABILITY ZONE: " . @file_get_contents("http://instance-data/latest/meta-data/placement/availability-zone");
-      echo "<br>";
-      echo "IP ADDRESS: " . @file_get_contents("http://instance-data/latest/meta-data/public-ipv4");
- ?>
+    $url = "http://169.254.169.254/latest/meta-data/instance-id";   
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    $res = curl_exec($ch);
+    echo $res;
+?>
